9ac3d31b9a1d907bc92d69d8a166f4f7cb8e509b
Nathan Wang <nathan.r.wang@gmail.com>
2018-10-03 17:51:57 -0700

    feat: initial commit

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidGhlY29kaW5nd2l6YXJkIiwiYSI6ImNqbXR1dnh1ZTAwOWkzd3J1d2doeWFyd3cifQ.vA8nYwNIcVGV3HHrINeiIg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
